947eafd50db5ff329d58d5455661f5e8a54e1240
SZossimov <szossimov@beeline.kz>
2026-08-30 16:47:31 +0500

    iter4

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,10 +1,57 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
 	"github.com/sergezossimov0/yndx-metrics-alerting-service.git/internal/agent"
 )
 
 func main() {
-	newAgent := agent.NewAgent("http://localhost:8080", 2, 10)
+	fs := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+	fs.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Agent startup options:")
+		fs.PrintDefaults()
+	}
+
+	serverAddr := fs.String("a", "localhost:8080", "HTTP server address")
+	reportInterval := fs.Int("r", 10, "report interval in seconds")
+	pollInterval := fs.Int("p", 2, "poll interval in seconds")
+
+	if err := fs.Parse(normalizeHelpArg(os.Args[1:])); err != nil {
+		if errors.Is(err, flag.ErrHelp) {
+			os.Exit(0)
+		}
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	baseURL := normalizeServerAddr(*serverAddr)
+	log.Printf("agent starting with addr=%s poll_interval=%ds report_interval=%ds", baseURL, *pollInterval, *reportInterval)
+	newAgent := agent.NewAgent(baseURL, *pollInterval, *reportInterval)
 	newAgent.Run()
+}
+
+func normalizeHelpArg(args []string) []string {
+	normalized := make([]string, len(args))
+	copy(normalized, args)
+	for i, arg := range normalized {
+		if arg == "--help" {
+			normalized[i] = "-h"
+		}
+	}
+	return normalized
+}
+
+func normalizeServerAddr(addr string) string {
+	if strings.HasPrefix(addr, "http://") || strings.HasPrefix(addr, "https://") {
+		return addr
+	}
+	return "http://" + addr
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/sergezossimov0/yndx-metrics-alerting-service.git/internal/handler"
@@ -11,11 +16,28 @@ import (
 
 func main() {
 	if err := run(); err != nil {
-		panic(err)
+		if errors.Is(err, flag.ErrHelp) {
+			os.Exit(0)
+		}
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
 
 func run() error {
+	fs := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	fs.SetOutput(os.Stderr)
+	fs.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Server startup options:")
+		fs.PrintDefaults()
+	}
+
+	serverAddr := fs.String("a", "localhost:8080", "HTTP server address")
+	if err := fs.Parse(normalizeHelpArg(os.Args[1:])); err != nil {
+		return err
+	}
+
 	store := repository.NewMemStorage()
 	uc := usecase.NewMetricUpdate(store)
 	r := chi.NewRouter()
@@ -24,5 +46,17 @@ func run() error {
 	r.Get("/value/{type}/{name}", handler.GetMetricValueHandler(store))
 	r.Get("/", handler.ListMetricsHandler(store))
 
-	return http.ListenAndServe(":8080", r)
+	log.Printf("server starting on addr=%s", *serverAddr)
+	return http.ListenAndServe(*serverAddr, r)
+}
+
+func normalizeHelpArg(args []string) []string {
+	normalized := make([]string, len(args))
+	copy(normalized, args)
+	for i, arg := range normalized {
+		if arg == "--help" {
+			normalized[i] = "-h"
+		}
+	}
+	return normalized
 }
